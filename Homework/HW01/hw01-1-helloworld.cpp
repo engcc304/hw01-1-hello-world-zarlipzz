825bd6cb135c0eb4a3203562d9b30d94e41ef69f
zarlipzz <guitar30255@gmail.com>
2023-06-29 22:28:09 +0700

    HW1 SRWH

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -1,5 +1,7 @@
-//TODO: #3 provide me full syntax of C-Language contain include, main function(int).
+#include <stdio.h> 
+ 
+int main() {
+    printf( "Hello World." ) ; 
+    return 0 ;
 
-//TODO: #4 display me "Hello World." to me.
-
-//TODO: #5 Write your private info into Student-Info.txt
+ }
